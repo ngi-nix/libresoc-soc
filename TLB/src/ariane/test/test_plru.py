9370cfab6194d7a7279be18015b54871e82c5645
Daniel Benusovich <flyingmonkeys1996@gmail.com>
2019-05-04 15:36:59 -0700

    Add test setup. It compiles

--- TLB/src/ariane/test/test_plru.py
+++ TLB/src/ariane/test/test_plru.py
@@ -2,4 +2,14 @@ import sys
 sys.path.append("../src")
 sys.path.append("../../../TestUtil")
 
+from plru import PLRU
+
 from nmigen.compat.sim import run_simulation
+
+def testbench(dut):
+    yield
+
+if __name__ == "__main__":
+    dut = PLRU(4)
+    run_simulation(dut, testbench(dut), vcd_name="test_plru.vcd")
+    print("PLRU Unit Test Success")
