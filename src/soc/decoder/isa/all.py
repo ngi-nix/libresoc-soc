1f4855d84c5f6980653356fac8146d1e3a6c1cb7
Michael Nolan <mtnolan2640@gmail.com>
2020-04-05 15:10:43 -0400

    Fix not being able to use all instructions in ISA class

--- src/soc/decoder/isa/all.py
+++ src/soc/decoder/isa/all.py
@@ -1,8 +1,19 @@
+from caller import ISACaller
 from fixedarith import fixedarith
 from fixedload import fixedload
 from fixedstore import fixedstore
 
 
 
-class ISA(fixedarith, fixedload, fixedstore):
-    pass
+class ISA(ISACaller):
+    def __init__(self, dec, regs):
+        super().__init__(dec, regs)
+        self.fixedarith = fixedarith(dec, regs)
+        self.fixedload = fixedload(dec, regs)
+        self.fixedstore = fixedstore(dec, regs)
+
+        self.instrs = {
+            **self.fixedarith.instrs,
+            **self.fixedload.instrs,
+            **self.fixedstore.instrs,
+        }
