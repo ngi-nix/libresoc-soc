dde610f718c890e29419f1bdbf2f9f2e428866f9
Michael Nolan <mtnolan2640@gmail.com>
2020-05-13 15:54:55 -0400

    Add support for OP_EXTS

--- src/soc/alu/main_stage.py
+++ src/soc/alu/main_stage.py
@@ -61,6 +61,17 @@ class ALUMainStage(PipeModBase):
             #### xor ####
             with m.Case(InternalOp.OP_XOR):
                 comb += self.o.o.eq(self.i.a ^ self.i.b)
+            with m.Case(InternalOp.OP_EXTS):
+                with m.If(self.i.ctx.op.data_len == 1):
+                    comb += self.o.o.eq(Cat(self.i.a[0:8],
+                                            Repl(self.i.a[7], 64-8)))
+                with m.If(self.i.ctx.op.data_len == 2):
+                    comb += self.o.o.eq(Cat(self.i.a[0:16],
+                                            Repl(self.i.a[15], 64-16)))
+                with m.If(self.i.ctx.op.data_len == 4):
+                    comb += self.o.o.eq(Cat(self.i.a[0:32],
+                                            Repl(self.i.a[31], 64-32)))
+                    
                 
 
         ###### sticky overflow and context, both pass-through #####
--- src/soc/alu/test/test_pipe_caller.py
+++ src/soc/alu/test/test_pipe_caller.py
@@ -151,6 +151,16 @@ class ALUTestCase(FHDLTestCase):
         initial_regs[7] = random.randint(0, (1<<64)-1)
         self.run_tst_program(Program(lst), initial_regs, {})
 
+    def test_extsb(self):
+        insns = ["extsb", "extsh", "extsw"]
+        for i in range(10):
+            choice = random.choice(insns)
+            lst = [f"{choice} 3, 1"]
+            print(lst)
+            initial_regs = [0] * 32
+            initial_regs[1] = random.randint(0, (1<<64)-1)
+            self.run_tst_program(Program(lst), initial_regs)
+
     def test_ilang(self):
         rec = CompALUOpSubset()
 
