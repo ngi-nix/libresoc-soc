9772d66af3f4f97fd4e4787ecf1dbb61cbb0e6c3
Michael Nolan <mtnolan2640@gmail.com>
2020-05-20 11:15:10 -0400

    Add test for edge cases that were previously buggy

--- src/soc/fu/logical/test/test_pipe_caller.py
+++ src/soc/fu/logical/test/test_pipe_caller.py
@@ -153,6 +153,14 @@ class LogicalTestCase(FHDLTestCase):
             initial_regs[1] = random.randint(0, (1<<64)-1)
             self.run_tst_program(Program(lst), initial_regs)
 
+    def test_popcnt_edge(self):
+        insns = ["popcntb", "popcntw", "popcntd"]
+        for choice in insns:
+            lst = [f"{choice} 3, 1"]
+            initial_regs = [0] * 32
+            initial_regs[1] = -1
+            self.run_tst_program(Program(lst), initial_regs)
+
     def test_cmpb(self):
         lst = ["cmpb 3, 1, 2"]
         initial_regs = [0] * 32
