1aca708a10492637b144adc79a9351c37add6f0a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-05-22 18:51:08 +0100

    div probably uses ALU not Logical, needs double-checking though

--- src/soc/fu/div/pipe_data.py
+++ src/soc/fu/div/pipe_data.py
@@ -1,10 +1,10 @@
 from nmigen import Signal, Const
 from soc.fu.pipe_data import IntegerData
 from soc.fu.alu.pipe_data import ALUOutputData, CommonPipeSpec
-from soc.fu.logical.pipe_data import LogicalInputData
+from soc.fu.alu.pipe_data import ALUInputData # TODO: check this
 from soc.fu.logical.logical_input_record import CompLogicalOpSubset
 
 
 class DivPipeSpec(CommonPipeSpec):
-    regspec = (LogicalInputData.regspec, ALUOutputData.regspec)
+    regspec = (ALUInputData.regspec, ALUOutputData.regspec)
     opsubsetkls = CompLogicalOpSubset
