b1a9215b81b6f74edbe814ba71714fb9ca4cd32b
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-05-24 16:05:00 +0100

    add stub regfiles.py

---
+++ src/soc/regfile/regfiles.py
@@ -0,0 +1,19 @@
+# POWER9 Register Files
+"""POWER9 regfiles
+
+Defines the following register files:
+
+    * INT regfile
+    * SPR regfile
+    * CR regfile
+    * XER regfile
+    * FAST regfile
+
+Links:
+
+* https://bugs.libre-soc.org/show_bug.cgi?id=345
+* https://libre-soc.org/3d_gpu/architecture/regfile/
+* https://libre-soc.org/openpower/isatables/sprs.csv
+"""
+
+# TODO
