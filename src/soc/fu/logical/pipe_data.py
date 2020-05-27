fb434cfc91175bc80c39b3083ea3e5f96c835413
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-05-27 17:12:03 +0100

    LogicalOutputData does not need XER.so

--- src/soc/fu/logical/pipe_data.py
+++ src/soc/fu/logical/pipe_data.py
@@ -30,27 +30,25 @@ class LogicalOutputData(IntegerData):
     regspec = [('INT', 'o', '0:63'),
                ('CR', 'cr0', '0:3'),
                ('XER', 'xer_ca', '34,45'),
-               ('XER', 'xer_so', '32')]
+               ]
     def __init__(self, pspec):
         super().__init__(pspec)
         self.o = Data(64, name="stage_o")  # RT
         self.cr0 = Data(4, name="cr0")
         self.xer_ca = Data(2, name="xer_co") # bit0: ca, bit1: ca32
-        self.xer_so = Data(1, name="xer_so")
 
     def __iter__(self):
         yield from super().__iter__()
         yield self.o
         yield self.xer_ca
         yield self.cr0
-        yield self.xer_so
 
     def eq(self, i):
         lst = super().eq(i)
         return lst + [self.o.eq(i.o),
                       self.xer_ca.eq(i.xer_ca),
                       self.cr0.eq(i.cr0),
-                      self.xer_so.eq(i.xer_so)]
+                      ]
 
 
 class LogicalPipeSpec(CommonPipeSpec):
