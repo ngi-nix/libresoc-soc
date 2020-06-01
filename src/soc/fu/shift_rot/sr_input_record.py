00e615071482b20753911fda67c2c62803a3e513
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-06-01 20:16:25 +0100

    more unneeded fields from SR InputRecord

--- src/soc/fu/shift_rot/sr_input_record.py
+++ src/soc/fu/shift_rot/sr_input_record.py
@@ -14,9 +14,6 @@ class CompSROpSubset(Record):
         layout = (('insn_type', InternalOp),
                   ('fn_unit', Function),
                   ('imm_data', Layout((("imm", 64), ("imm_ok", 1)))),
-                    #'cr = Signal(32, reset_less=True) # NO: this is from the CR SPR
-                    #'xerc = XerBits() # NO: this is from the XER SPR
-                  ('lk', 1),
                   ('rc', Layout((("rc", 1), ("rc_ok", 1)))),
                   ('oe', Layout((("oe", 1), ("oe_ok", 1)))),
                   ('write_cr', Layout((("data", 3), ("ok", 1)))), # Data
@@ -27,25 +24,19 @@ class CompSROpSubset(Record):
                   ('is_32bit', 1),
                   ('is_signed', 1),
                   ('insn', 32),
-                  ('byte_reverse', 1),
-                  ('sign_extend', 1))
+                  )
 
         Record.__init__(self, Layout(layout), name=name)
 
         # grrr.  Record does not have kwargs
         self.insn_type.reset_less = True
         self.fn_unit.reset_less = True
-        #self.cr = Signal(32, reset_less = True
-        #self.xerc = XerBits(
-        self.lk.reset_less = True
         self.input_carry.reset_less = True
         self.output_carry.reset_less = True
         self.input_cr.reset_less = True
         self.output_cr.reset_less = True
         self.is_32bit.reset_less = True
         self.is_signed.reset_less = True
-        self.byte_reverse.reset_less = True
-        self.sign_extend.reset_less = True
 
     def eq_from_execute1(self, other):
         """ use this to copy in from Decode2Execute1Type
@@ -58,15 +49,10 @@ class CompSROpSubset(Record):
 
     def ports(self):
         return [self.insn_type,
-                #self.cr,
-                #self.xerc,
-                self.lk,
                 self.input_carry,
                 self.output_carry,
                 self.input_cr,
                 self.output_cr,
                 self.is_32bit,
                 self.is_signed,
-                self.byte_reverse,
-                self.sign_extend,
         ]
