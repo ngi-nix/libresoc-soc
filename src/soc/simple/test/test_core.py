790c8ad018b2048008d216666a33522682df0afb
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-06-07 16:03:12 +0100

    add msr to ISA in test_core.py

--- src/soc/simple/test/test_core.py
+++ src/soc/simple/test/test_core.py
@@ -77,7 +77,8 @@ class TestRunner(FHDLTestCase):
                 print(test.name)
                 program = test.program
                 self.subTest(test.name)
-                sim = ISA(pdecode2, test.regs, test.sprs, test.cr, test.mem)
+                sim = ISA(pdecode2, test.regs, test.sprs, test.cr, test.mem,
+                          test.msr)
                 gen = program.generate_instructions()
                 instructions = list(zip(gen, program.assembly.splitlines()))
 
