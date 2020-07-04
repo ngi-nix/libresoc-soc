458774fe981a2e4cb376e7febb615a2d3eb70ab4
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-07-04 10:50:31 +0100

    reduce steps per stage to 8

--- src/soc/fu/div/pipeline.py
+++ src/soc/fu/div/pipeline.py
@@ -38,7 +38,7 @@ class DivStagesEnd(PipeModBaseChain):
 
 
 class DIVBasePipe(ControlBase):
-    def __init__(self, pspec, compute_steps_per_stage=8):
+    def __init__(self, pspec, compute_steps_per_stage=4):
         ControlBase.__init__(self)
         self.pipe_start = DivStagesStart(pspec)
         compute_steps = pspec.core_config.n_stages
