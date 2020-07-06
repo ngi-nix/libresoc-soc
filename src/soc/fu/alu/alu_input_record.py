11dc865949693b3c92b6077880015c9e7e20bb0a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-07-06 23:22:45 +0100

    remove alu unneeded op record data

--- src/soc/fu/alu/alu_input_record.py
+++ src/soc/fu/alu/alu_input_record.py
@@ -14,7 +14,6 @@ class CompALUOpSubset(Record):
         layout = (('insn_type', InternalOp),
                   ('fn_unit', Function),
                   ('imm_data', Layout((("imm", 64), ("imm_ok", 1)))),
-                  ('lk', 1),
                   ('rc', Layout((("rc", 1), ("rc_ok", 1)))), # Data
                   ('oe', Layout((("oe", 1), ("oe_ok", 1)))), # Data
                   ('invert_a', 1),
@@ -29,7 +28,6 @@ class CompALUOpSubset(Record):
                   ('is_signed', 1),
                   ('data_len', 4), # actually used by ALU, in OP_EXTS
                   ('insn', 32),
-                  ('byte_reverse', 1),
                   ('sign_extend', 1))
 
         Record.__init__(self, Layout(layout), name=name)
@@ -37,7 +35,6 @@ class CompALUOpSubset(Record):
         # grrr.  Record does not have kwargs
         self.insn_type.reset_less = True
         self.fn_unit.reset_less = True
-        self.lk.reset_less = True
         self.zero_a.reset_less = True
         self.invert_a.reset_less = True
         self.invert_out.reset_less = True
@@ -48,7 +45,6 @@ class CompALUOpSubset(Record):
         self.is_32bit.reset_less = True
         self.is_signed.reset_less = True
         self.data_len.reset_less = True
-        self.byte_reverse.reset_less = True
         self.sign_extend.reset_less = True
 
     def eq_from_execute1(self, other):
@@ -62,7 +58,6 @@ class CompALUOpSubset(Record):
 
     def ports(self):
         return [self.insn_type,
-                self.lk,
                 self.invert_a,
                 self.invert_out,
                 self.input_carry,
@@ -72,6 +67,5 @@ class CompALUOpSubset(Record):
                 self.is_32bit,
                 self.is_signed,
                 self.data_len,
-                self.byte_reverse,
                 self.sign_extend,
         ]
