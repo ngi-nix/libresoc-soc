8546eb24e9ae7649dccb51f512b9269f29aa15c7
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-07-08 19:45:27 +0100

    add spr to fast reg converter

--- src/soc/regfile/util.py
+++ src/soc/regfile/util.py
@@ -12,3 +12,18 @@ def fast_reg_to_spr(spr_num):
         return SPR.SRR0.value
     elif spr_num == FastRegs.SRR1:
         return SPR.SRR1.value
+
+
+def spr_to_fast_reg(spr_num):
+    if not isinstance(spr_num, str):
+        spr_num = spr_dict[spr_num].SPR
+    if spr_num == 'CTR':
+        return FastRegs.CTR
+    elif spr_num == 'LR':
+        return FastRegs.LR
+    elif spr_num == 'TAR':
+        return FastRegs.TAR
+    elif spr_num == 'SRR0':
+        return FastRegs.SRR0
+    elif spr_num == 'SRR1':
+        return FastRegs.SRR1
