f7c9d276783bb8caf11d17710836f8f03be64f17
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-07-13 11:27:08 +0100

    add extswsli unit test

--- src/soc/fu/shift_rot/test/test_pipe_caller.py
+++ src/soc/fu/shift_rot/test/test_pipe_caller.py
@@ -141,6 +141,14 @@ class ShiftRotTestCase(FHDLTestCase):
         initial_regs[1] = random.randint(0, (1<<64)-1)
         self.run_tst_program(Program(lst, bigendian), initial_regs)
 
+    def test_extswsli(self):
+        for i in range(40):
+            sh = random.randint(0, 63)
+            lst = [f"extswsli 3, 1, {sh}"]
+            initial_regs = [0] * 32
+            initial_regs[1] = random.randint(0, (1<<64)-1)
+            self.run_tst_program(Program(lst, bigendian), initial_regs)
+
     def test_rlc(self):
         insns = ["rldic", "rldicl", "rldicr"]
         for i in range(20):
