7bc0587c6fd447e1b4abe30c95d01bd3a5a1af02
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-07-16 10:47:16 +0100

    use CompOpSubsetBase class in Branch op record

--- src/soc/fu/branch/br_input_record.py
+++ src/soc/fu/branch/br_input_record.py
@@ -1,14 +1,11 @@
-from nmigen.hdl.rec import Record, Layout
+from soc.fu.base_input_record import CompOpSubsetBase
+from nmigen.hdl.rec import Layout
 
-from soc.decoder.power_enums import MicrOp, Function, CryIn
+from soc.decoder.power_enums import MicrOp, Function
 
 
-class CompBROpSubset(Record):
+class CompBROpSubset(CompOpSubsetBase):
     """CompBROpSubset
-
-    TODO: remove anything not needed by the Branch pipeline (determine this
-    after all branch operations have been written.  see
-    https://bugs.libre-soc.org/show_bug.cgi?id=313#c3)
 
     a copy of the relevant subset information from Decode2Execute1Type
     needed for Branch operations.  use with eq_from_execute1 (below) to
@@ -17,34 +14,11 @@ class CompBROpSubset(Record):
     def __init__(self, name=None):
         layout = (('insn_type', MicrOp),
                   ('fn_unit', Function),
+                  ('insn', 32),
                   ('imm_data', Layout((("imm", 64), ("imm_ok", 1)))),
                   ('lk', 1),
                   ('is_32bit', 1),
-                  ('insn', 32),
                   )
 
-        Record.__init__(self, Layout(layout), name=name)
+        super().__init__(layout, name=name)
 
-        # grrr.  Record does not have kwargs
-        self.insn_type.reset_less = True
-        self.fn_unit.reset_less = True
-        self.lk.reset_less = True
-        self.is_32bit.reset_less = True
-        self.insn.reset_less = True
-
-    def eq_from_execute1(self, other):
-        """ use this to copy in from Decode2Execute1Type
-        """
-        res = []
-        for fname, sig in self.fields.items():
-            eqfrom = other.do.fields[fname]
-            res.append(sig.eq(eqfrom))
-        return res
-
-    def ports(self):
-        return [self.insn_type,
-                self.fn_unit,
-                self.lk,
-                self.is_32bit,
-                self.insn,
-        ]
