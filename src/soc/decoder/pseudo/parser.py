b635fd5c2822f7c54675b5a55df15908a99b1d5a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-08-14 20:44:35 +0100

    bug in isa parser not recognising MSR as declared variable

--- src/soc/decoder/pseudo/parser.py
+++ src/soc/decoder/pseudo/parser.py
@@ -395,7 +395,8 @@ class PowerParser:
                     if name in self.gprs:
                         # add to list of uninitialised
                         self.uninit_regs.add(name)
-                    autoassign = name not in self.declared_vars
+                    autoassign = (name not in self.declared_vars and
+                                  name not in self.special_regs)
             elif isinstance(p[1], ast.Call) and p[1].func.id in ['GPR', 'SPR']:
                 print(astor.dump_tree(p[1]))
                 # replace GPR(x) with GPR[x]
