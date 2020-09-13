6ffa5a61d7cf6e1c9aec8bb519b95347efe9cbd4
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-09-13 18:42:36 +0100

    last mmu get seems ok

--- src/soc/experiment/mmu.py
+++ src/soc/experiment/mmu.py
@@ -529,6 +529,11 @@ def dcache_get(dut):
                         # V = 1 L = 0 NLB = 0x400 NLS = 9
            b(0x8000000000040009),
 
+           0x40000:     # RADIX_SECOND_LEVEL
+                        # 	   V = 1 L = 1 SW = 0 RPN = 0
+	                    # R = 1 C = 1 ATT = 0 EAA 0x7
+           b(0xc000000000000187),
+
           0x1000000:   # PROCESS_TABLE_3
                        # RTS1 = 0x2 RPDB = 0x300 RTS2 = 0x5 RPDS = 13
            b(0x40000000000300ad),
