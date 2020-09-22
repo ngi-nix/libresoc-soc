6e9736160db60a86c1ea1b56acfde2e4d24db9be
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-09-22 12:46:59 +0100

    add similar platforms to ls180.py

--- src/soc/litex/florent/libresoc/ls180.py
+++ src/soc/litex/florent/libresoc/ls180.py
@@ -4,6 +4,15 @@
 # Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
+"""ls180 ASIC platform
+
+conceptually similar to the following:
+
+* https://github.com/enjoy-digital/liteeth/blob/master/liteeth/gen.py
+* https://github.com/enjoy-digital/litepcie/blob/master/litepcie/gen.py
+
+"""
+
 from migen.fhdl.structure import _Fragment
 from litex.build.generic_platform import (GenericPlatform, Pins,
                                         Subsignal, IOStandard, Misc,
