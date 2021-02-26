755e84aba04a4cc02e535fcd269d126b7d4f556a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-02-26 13:50:04 +0000

    comment on CoreState

--- src/soc/config/state.py
+++ src/soc/config/state.py
@@ -4,6 +4,13 @@ from soc.sv.svstate import SVSTATERec
 
 
 class CoreState(RecordObject):
+    """contains "Core State Information" which says exactly where things are
+
+    example: eint says to PowerDecoder that it should fire an exception
+    rather than let the current decoded instruction proceed.  likewise
+    if dec goes negative.  MSR contains LE/BE and Priv state.  PC contains
+    the Program Counter, and SVSTATE is the Sub-Program-Counter.
+    """
     def __init__(self, name):
         super().__init__(name=name)
         self.pc = Signal(64)      # Program Counter (CIA, NIA)
