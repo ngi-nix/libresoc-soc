aa0df7f05936ec4dca90d2292498d1c0c32a9751
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-05-09 20:09:45 +0100

    remove invalid setting of d_in.valid from self.mmureq

--- src/soc/fu/ldst/loadstore.py
+++ src/soc/fu/ldst/loadstore.py
@@ -246,7 +246,6 @@ class LoadStore1(PortInterfaceBase):
         m.d.comb += d_in.byte_sel.eq(self.byte_sel)
         m.d.comb += d_in.addr.eq(self.addr)
         m.d.comb += d_in.nc.eq(self.nc)
-        m.d.comb += d_in.valid.eq(self.mmureq)
         m.d.comb += self.done.eq(d_out.valid)
         m.d.comb += self.load_data.eq(d_out.data)
 
