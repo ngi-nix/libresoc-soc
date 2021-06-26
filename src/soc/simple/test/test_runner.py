53430a7d9b7fe35dce3850cf74559ecdb11ff6d5
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-06-26 13:38:03 +0100

    use standard create_pdecode in TestRunner

--- src/soc/simple/test/test_runner.py
+++ src/soc/simple/test/test_runner.py
@@ -169,9 +169,8 @@ class TestRunner(FHDLTestCase):
         l0 = core.l0
         regreduce_en = pspec.regreduce_en == True
 
-        # copy of the decoder for simulator
-        simdec = create_pdecode()
-        simdec2 = PowerDecode2(simdec, regreduce_en=regreduce_en)
+        #simdec = create_pdecode()
+        simdec2 = PowerDecode2(None, regreduce_en=regreduce_en)
         m.submodules.simdec2 = simdec2  # pain in the neck
 
         # run core clock at same rate as test clock
